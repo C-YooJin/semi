184902a675d88931fcabdf0a5a2b00ac2f3388a0
C-YooJin <ioi_3312@naver.com>
2020-06-11 20:24:40 +0900

    Add Dto test code

--- src/test/java/com/deepjin/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/deepjin/book/springboot/web/HelloControllerTest.java
@@ -12,6 +12,10 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+
 @RunWith(SpringRunner.class)
 @WebMvcTest
 public class HelloControllerTest {
@@ -27,4 +31,19 @@ public class HelloControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
     }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                .param("name", name)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
+
 }
