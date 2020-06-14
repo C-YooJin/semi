cef5457649b5662531485bd2548e956a7081b37d
C-YooJin <ioi_3312@naver.com>
2020-06-14 19:50:04 +0900

    Create Service and PostsApiController

---
+++ src/main/java/com/deepjin/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.deepjin.book.springboot.service.posts;
+
+import com.deepjin.book.springboot.domain.posts.PostsRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.deepjin.book.springboot.web.dto.PostsSaveRequestDto;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
