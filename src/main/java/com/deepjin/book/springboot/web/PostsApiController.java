2981fc6254403a6943efbc36a77a2a4da6c61b25
C-YooJin <ioi_3312@naver.com>
2020-07-06 22:19:13 +0900

    Fix PostsApiController

--- src/main/java/com/deepjin/book/springboot/web/PostsApiController.java
+++ src/main/java/com/deepjin/book/springboot/web/PostsApiController.java
@@ -3,6 +3,7 @@ package com.deepjin.book.springboot.web;
 import com.deepjin.book.springboot.service.posts.PostsService;
 import com.deepjin.book.springboot.web.dto.PostsResponseDto;
 import com.deepjin.book.springboot.web.dto.PostsSaveRequestDto;
+import com.deepjin.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,19 +13,14 @@ public class PostsApiController {
 
     private final PostsService postsService;
 
-    public PostsApiController(PostsService postsService) {
-        this.postsService = postsService;
-    }
-
-    @PostMapping("/api/v1/posts")
-    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+    @PutMapping("/api/v1/posts")
+    public Long save (@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
     }
 
     @PutMapping("/api/v1/posts/{id}")
-    public Long update(@PathVariable Long id, @RequestBody PostsSaveRequestDto requestDto) {
-        return postsService.update(id, requestDto);
-    }
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto); }
 
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById (@PathVariable Long id) {
